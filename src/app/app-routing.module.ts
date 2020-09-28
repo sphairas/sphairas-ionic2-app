b67f8df6e20257eb2efde72f2ff10deffb8e78f1
Boris Heithecker <boris.heithecker@gmx.net>
2020-09-28 19:58:22 +0200

    Log out option

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule),
+    canActivate: [AuthGuardService]
   },
   {
     path: '',
